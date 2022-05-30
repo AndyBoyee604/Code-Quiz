2e8b81336232a9896242db8e2eea06347ae17b6c
AndyBoyee <n.andyvue@outlook.com>
2022-05-30 00:00:18 -0500

    done

--- README.md
+++ README.md
@@ -12,12 +12,13 @@ JS
 [Coding Quiz Challenge](https://andyboyee604.github.io/Code-Quiz/)
 
 ## Usage
-![alt text]()
+![alt text](./assets/images/1st%20page.jpg)
 
 ## Sources Referenced
 [Google](https://www.google.com) **|**
 [MDN Web Docs](https://developer.mozilla.org/en-US) **|**
 [Tutorialspoint](https://www.tutorialspoint.com/index.htm) **|**
-[w3 Schools](https://www.w3schools.com)
+[w3 Schools](https://www.w3schools.com) **|**
+[GetBootStrap](https://getbootstrap.com/docs/4.0/getting-started/introduction/)
 
 ## Good Luck Testing!
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,29 @@
+// variables for timer
 var timer = document.querySelector(".timer");
 var timeInterval;
 var timeLeft = 75;
-var startQuiz = document.querySelector("#startQuiz");
+// varibales for question
+var start = document.querySelector("#startQuiz");
 var question = document.querySelector("#question");
 var head = document.querySelector(".head");
+var title = document.querySelector(".title");
+var quizSection = document.querySelector("#quizSection");
+var viewHsBtn = document.querySelector(".btn")
+var quest = 0;
 
-var quest = 0
+var selection = document.querySelector("#multipleChoice")
 
 // timer for quiz, start
-var startTimer = function () {
+function startTimer() {
   timeInterval = setInterval(function () {
     timeLeft--;
-    timer.textContent = "TIMER:" + timeLeft
+    timer.textContent = "TIMER:" + timeLeft;
+
+    if (timeLeft === 0 || quest >= quizQuestions.length) {
+      clearInterval(timeInterval);
+    }
   }, 1000);
-};
+}
 
 // show question and choices
 function showQC() {
@@ -27,15 +37,98 @@ function showQC() {
 }
 
 //event listeners
-startQuiz.addEventListener("click", function () {
+start.addEventListener("click", function () {
   startTimer();
   showQC();
-  head.classList.add("hide");
-  
-
+  head.style.display = "none";
+  title.style.display = "none";
+  quizSection.classList.remove("hide");
 });
 
-//Questions
+
+
+//right or wrong answer clicked
+var answer = document.querySelector("#answers")
+function rightAnswer(event) {
+  if (quest >= quizQuestions.length) {
+    finished();
+    clearInterval(timeInterval);
+  } else {
+    if (event === quizQuestions[quest].answer) {
+      answer.textContent = "Correct!"
+     saveLocal()
+    } else {
+      timeLeft -=2;
+      answer.textContent = "Wrong!"
+    }
+    score = timeLeft;
+    quest++;
+    showQC();
+  }
+}
+selection.addEventListener("click", function (event) {
+  var event = event.target;
+  rightAnswer(event.textContent.trim());
+});
+
+//change finsihed
+var finished = function() {
+  selection.remove("#multipleChoice");
+  quizSection.remove("#quizSection");
+  answer.remove("#answer")
+
+}
+
+//local saves
+var submit = document.querySelector("#submit")
+var keepScore = 1
+
+var saveLocal = function () {
+  if (answer.textContent === "correct!") {
+    localStorage.setItem("score", JSON.stringify(keepScore))
+    keepScore++;
+  }
+};
+var user = function () {
+  var name = document.querySelector("#player")
+  var playerName = (name).value;
+  localStorage.setItem("name", JSON.stringify(playerName))
+};
+var HsBoard = function () {
+  var shwlead = document.querySelector(".scoreSection")
+  shwlead.style.display = "block"
+}
+
+submit.addEventListener("click", function () {
+  user()
+  hide()
+  HsBoard()
+});
+
+var player = document.querySelector(".HsSection")
+var info = function () {
+  player.style.display = "block"
+}
+var hide = function () {
+  player.style.display = "none"
+}
+
+var userName = localStorage.getItem("name")
+var userScore = localStorage.getItem("score")
+var scoreBoard = function () {
+  var userInfo = document.querySelector("#playerScore")
+  userInfo.textContent = userName + userScore;
+}
+
+var finished = function () {
+  selection.remove("#choices")
+  question.remove("question")
+  info()
+  scoreBoard()
+}
+
+
+//Questions and Choices
 var quizQuestions = [
   {
     question: "Commonly used data types DO Not Include:",
@@ -44,22 +137,22 @@ var quizQuestions = [
   },
   {
     question: "The condition in an if / else statement is enclosed with ______.",
-    selection: ["True", "False", "Maybe", "I don't know"],
-    answer: "False"
+    selection: ["quotes", "curly brackets", "paranthesis", "square brackets"],
+    answer: "paranthesis"
   },
   {
     question: "Arrays in JavaScript can be used to store ______.",
-    selection: ["<script>", "<js>", "<javascript>", "<JS>"],
-    answer: "<script>"
+    selection: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    answer: "all of the above"
   },
   {
     question: "String values must be enclosed within ______ when being assigned to variables.",
-    selection: ["if i = 5", "if (i == 5)", "if i = 5 then", "if i == 5 then"],
-    answer: "if (i == 5) "
+    selection: ["commas", "curly brackets", "quotes", "paranthesis"],
+    answer: "quotes"
   },
   {
     question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-    selection: ["<script name='./xxx.js'>", "<script href='./xxx.js'>", "<script scr='./xxx.js'>", "<script ='./xxx.js'>"],
-    answer: "<script scr='./xxx.js'>"
+    selection: ["JavaScript", "terminal/bash", "for loops", "console log"],
+    answer: "console log"
   },
 ];
